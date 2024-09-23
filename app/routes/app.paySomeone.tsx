54614e7a80da8155e2dc26a9b102177c2c3cd8f4
Barradism <kfaysal@student.unimelb.edu.au>
2024-09-23 22:14:42 +1000

    Fixed Amount Bar Glitch for Pay Someone Page

--- app/routes/app.paySomeone.tsx
+++ app/routes/app.paySomeone.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, useState } from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react'; 
 import { Page, Card, Button, Input, Textarea, Text } from '@geist-ui/react';
 import { Select, Tabs } from '@geist-ui/core';
 import { Account, TransactionType } from '@prisma/client';
@@ -14,8 +14,7 @@ export const action: ActionFunction = async ({ context, request }: { context: an
     const amount = parseInt(formData.get('amount') as string);
     const reference = formData.get('reference') as string;
     const description = formData.get('description') as string;
-    //adendiamond1@gmail.com
-    // parse the recipientAddress JSON string
+
     let recipient;
     try {
         recipient = JSON.parse(recipientAddress);
@@ -28,8 +27,7 @@ export const action: ActionFunction = async ({ context, request }: { context: an
 
     try {
         const result = await db.$transaction(async (prisma) => {
-            console.log('fa', fromAcc)
-            // fetch the sender account
+            console.log('fa', fromAcc);
             const fromAccount = await prisma.account.findFirst({
                 where: { acc: fromAcc },
             });
@@ -38,11 +36,9 @@ export const action: ActionFunction = async ({ context, request }: { context: an
                 throw new Error('Sender account not found');
             }
 
-            // fetch the recipient account based on recipient data
             const toAccount = await prisma.account.findFirst({
                 where: {
-                    pay_id: recipient.payId
-
+                    pay_id: recipient.payId,
                 },
             });
 
@@ -54,12 +50,11 @@ export const action: ActionFunction = async ({ context, request }: { context: an
                 throw new Error('Cannot transfer to the same account');
             }
 
-            console.log(fromAccount.balance, amount)
+            console.log(fromAccount.balance, amount);
             if (fromAccount.balance < amount) {
                 throw new Error('Insufficient funds');
             }
 
-            // Update the account balances
             await prisma.account.update({
                 where: { acc: fromAccount.acc },
                 data: { balance: { decrement: amount } },
@@ -70,7 +65,6 @@ export const action: ActionFunction = async ({ context, request }: { context: an
                 data: { balance: { increment: amount } },
             });
 
-            // Create a new transaction
             const newTransaction = await prisma.transaction.create({
                 data: {
                     amount,
@@ -83,7 +77,7 @@ export const action: ActionFunction = async ({ context, request }: { context: an
                     type: TransactionType.paySomeone,
                     recipient_address: recipientAddress,
                     sender: { connect: { acc: fromAccount.acc } },
-                    recipient: { connect: { acc: toAccount.acc } }
+                    recipient: { connect: { acc: toAccount.acc } },
                 },
             });
 
@@ -98,11 +92,9 @@ export const action: ActionFunction = async ({ context, request }: { context: an
 };
 
 export const loader: LoaderFunction = async ({ context, request }: { context: any, request: Request }) => {
-    // Ensure the user is authenticated
     const user = await requireUserSession(request);
     const db = getPrismaClient(context);
 
-    // Fetch the user details and related data from Prisma
     const [userAccounts] = await Promise.all([
         db.account.findMany({
             where: { uid: user.uid },
@@ -122,7 +114,7 @@ const PaySomeone = () => {
     const actionData: any = useActionData();
     const { userAccounts: accounts } = useLoaderData<{ userAccounts: Account[] }>();
     const [fromAcc, setFromAcc] = useState<number | undefined>(undefined);
-    const [amount, setAmount] = useState(0);
+    const [amount, setAmount] = useState('');
     const [recipientAddress, setRecipientAddress] = useState<{
         accountName: string,
         acc: number,
@@ -136,7 +128,7 @@ const PaySomeone = () => {
         bsb: -1,
         payId: '',
         billerCode: -1,
-        crn: -1
+        crn: -1,
     });
 
     const [reference, setReference] = useState('');
@@ -148,22 +140,23 @@ const PaySomeone = () => {
 
     const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         let inputValue = event.target.value;
-        inputValue = inputValue.replace(/[^0-9.]/g, '');
-        //TODO fix this
 
-        // Ensure only one decimal point is allowed
+        // Remove any non-numeric characters except for the decimal point
+        inputValue = inputValue.replace(/[^0-9.]/g, '');
+
+        // Ensure there's only one decimal point
         const parts = inputValue.split('.');
         if (parts.length > 2) {
             inputValue = parts[0] + '.' + parts.slice(1).join('');
         }
 
-        // Format the number to 2 decimal places
-        if (inputValue) {
-            const amount = parseInt(inputValue.replace('.', ''))
-            setAmount(amount);
-        } else {
-            setAmount(0);
+        // Handle decimals and prevent multiple trailing zeros
+        if (parts.length === 2 && parts[1].length > 2) {
+            inputValue = parts[0] + '.' + parts[1].slice(0, 2);  // Limit to two decimal places
         }
+
+        // Update the amount state with the properly formatted input
+        setAmount(inputValue);
     };
 
     const toDigits = (value: string): number => {
@@ -173,10 +166,10 @@ const PaySomeone = () => {
         } else {
             return 0;
         }
-    }
+    };
 
     const updateRecipientAddress = (key: string, value: string | number) => {
-        setRecipientAddress(prevState => ({
+        setRecipientAddress((prevState) => ({
             ...prevState,
             [key]: value,
         }));
@@ -234,7 +227,13 @@ const PaySomeone = () => {
                         </Tabs>
                         <Text h4>From Account</Text>
                         <div style={{ width: '48%' }}>
-                            <Select placeholder="Select account" width="100%" onChange={handleFromAccChange} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} >
+                            <Select
+                                placeholder="Select account"
+                                width="100%"
+                                onChange={handleFromAccChange}
+                                onPointerEnterCapture={undefined}
+                                onPointerLeaveCapture={undefined}
+                            >
                                 {// @ts-ignore
                                 accounts.map((account: Account) => (
                                     <Select.Option key={account.acc} value={account.acc.toString()}>
@@ -247,7 +246,16 @@ const PaySomeone = () => {
                         <Tabs initialValue="acc-bsb" hideDivider style={{ marginTop: 20 }}>
                             <Tabs.Item label="ACC / BSB" value="acc-bsb">
                                 <Text h4>Account Name</Text>
-                                <Input width="100%" placeholder="Enter account name" aria-label="Account Name" value={recipientAddress.accountName} onChange={handleAccountNameChange} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+                                <Input
+                                    width="100%"
+                                    placeholder="Enter account name"
+                                    aria-label="Account Name"
+                                    value={recipientAddress.accountName}
+                                    onChange={handleAccountNameChange}
+                                    onPointerEnterCapture={undefined}
+                                    onPointerLeaveCapture={undefined}
+                                    crossOrigin={undefined}
+                                />
                                 <Text h4 style={{ marginTop: 10 }}>Account Number</Text>
                                 <Input
                                     width="100%"
@@ -255,7 +263,10 @@ const PaySomeone = () => {
                                     aria-label="Account Number"
                                     value={recipientAddress.acc == -1 ? '' : recipientAddress.acc.toString()}
                                     onChange={handleAccChange}
-                                    onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+                                    onPointerEnterCapture={undefined}
+                                    onPointerLeaveCapture={undefined}
+                                    crossOrigin={undefined}
+                                />
                                 <Text h4 style={{ marginTop: 10 }}>BSB</Text>
                                 <Input
                                     width="100%"
@@ -263,11 +274,22 @@ const PaySomeone = () => {
                                     aria-label="BSB"
                                     value={recipientAddress.bsb == -1 ? '' : recipientAddress.bsb.toString()}
                                     onChange={handleBsbChange}
-                                    onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+                                    onPointerEnterCapture={undefined}
+                                    onPointerLeaveCapture={undefined}
+                                    crossOrigin={undefined}
+                                />
                             </Tabs.Item>
                             <Tabs.Item label="PayID" value="pay-id">
                                 <Text h4>PayID</Text>
-                                <Input width="100%" placeholder="Enter PayID" aria-label="PayID" onChange={handlePayIdChange} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+                                <Input
+                                    width="100%"
+                                    placeholder="Enter PayID"
+                                    aria-label="PayID"
+                                    onChange={handlePayIdChange}
+                                    onPointerEnterCapture={undefined}
+                                    onPointerLeaveCapture={undefined}
+                                    crossOrigin={undefined}
+                                />
                             </Tabs.Item>
                             <Tabs.Item label="BPay" value="b-pay">
                                 <Text h4>Biller Code</Text>
@@ -277,7 +299,10 @@ const PaySomeone = () => {
                                     aria-label="Biller Code"
                                     value={recipientAddress.billerCode == -1 ? '' : recipientAddress.billerCode.toString()}
                                     onChange={handleBillerCodeChange}
-                                    onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+                                    onPointerEnterCapture={undefined}
+                                    onPointerLeaveCapture={undefined}
+                                    crossOrigin={undefined}
+                                />
                                 <Text h4 style={{ marginTop: 10 }}>CRN</Text>
                                 <Input
                                     width="100%"
@@ -285,22 +310,42 @@ const PaySomeone = () => {
                                     aria-label="CRN"
                                     value={recipientAddress.crn == -1 ? '' : recipientAddress.crn.toString()}
                                     onChange={handleCrnChange}
-                                    onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+                                    onPointerEnterCapture={undefined}
+                                    onPointerLeaveCapture={undefined}
+                                    crossOrigin={undefined}
+                                />
                             </Tabs.Item>
                         </Tabs>
                         <input type="hidden" name="recipientAddress" value={JSON.stringify(recipientAddress)} />
                         <Text h4 style={{ marginTop: 10 }}>Amount</Text>
-                        <Input width="100%" placeholder="Enter amount" aria-label="Amount" name="amount" value={amount.toFixed(2)} onChange={handleAmountChange} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+                        <Input
+                            width="100%"
+                            placeholder="Enter amount"
+                            aria-label="Amount"
+                            name="amount"
+                            value={amount}
+                            onChange={handleAmountChange}
+                            onPointerEnterCapture={undefined}
+                            onPointerLeaveCapture={undefined}
+                            crossOrigin={undefined}
+                        />
                         <Text h4 style={{ marginTop: 10 }}>Reference</Text>
-                        <Textarea width="100%" placeholder="Enter reference" aria-label="Reference" onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} />
+                        <Textarea
+                            width="100%"
+                            placeholder="Enter reference"
+                            aria-label="Reference"
+                            onPointerEnterCapture={undefined}
+                            onPointerLeaveCapture={undefined}
+                        />
                         <Text h4 style={{ marginTop: 10 }}>Description</Text>
-                        <Textarea width="100%" placeholder="Enter description" aria-label="Description" onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} />
-                        <div style={{
-                            display: 'flex',
-                            gap: 20,
-                            justifyContent: 'flex-end',
-                            marginTop: 20
-                        }}>
+                        <Textarea
+                            width="100%"
+                            placeholder="Enter description"
+                            aria-label="Description"
+                            onPointerEnterCapture={undefined}
+                            onPointerLeaveCapture={undefined}
+                        />
+                        <div style={{ display: 'flex', gap: 20, justifyContent: 'flex-end', marginTop: 20 }}>
                             <Button auto placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>Cancel</Button>
                             <Button auto htmlType="submit" type="secondary" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>Confirm</Button>
                         </div>
