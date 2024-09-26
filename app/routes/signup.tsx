1a5a3d22af92a0a0c79a66036c3991a78240d561
Aden Diamond <akdiamond@student.unimelb.edu.au>
2024-09-26 19:22:53 +1000

    Rebuilt login and sign in pages, applied a global gradient

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -4,13 +4,12 @@ import { Form, useActionData, useSubmit, useNavigation } from "@remix-run/react"
 import { commitSession, getSession } from "~/auth.server";
 import { getPrismaClient } from "~/util/db.server";
 import { openAccount } from "~/util/accountUtil";
-import { Page, Card, Button, Input, Textarea, Text } from '@geist-ui/react';
+import { Page, Card, Button, Input, Image, Text } from '@geist-ui/react';
 import { Select, Tabs } from '@geist-ui/core';
 import { signup } from "~/auth.client";
 import { useEffect, useState } from "react";
 import { createUser } from "~/util/userUtil";
 import { Link } from 'react-router-dom';
-import "../styles/signup.css";
 
 export const action: ActionFunction = async ({ context, request }: { context: any, request: Request }) => {
     const formData = await request.formData();
@@ -68,51 +67,25 @@ export default function Signup() {
     };
 
     return (
-        <div>
-            <h1 style={{
-            fontWeight: 'bold',
-            fontSize: '25px',
-            position:'relative',
-            right:'120px',
-            top:'40px'
-            }}className="sign">Sign Up</h1>
-            <Card style={{ width: '800px', height: '700px',display: 'flex',
-                alignItems: 'center', 
-                flexDirection: 'column', 
-                justifyContent: 'center',
-                 marginTop: '70px' 
-                }}>
-            <Form style={{display: 'flex',
-                 alignItems: 'center', 
-                flexDirection: 'column', 
-                justifyContent: 'flex-start'}}method="post" onSubmit={handleSubmit}>
-                <div className="first-name-container">
-                    <input type="text" name="first_name" placeholder="First Name" required />
-                </div>
-                <div className="last-name-container">
-                    <input type="text" name="last_name" placeholder="Last Name"  required />
-                </div>
-                <div className="emailS-container">
-                    <input type="email" name="email" placeholder="Enter Email" required />
-                </div>
-                <div className="passwordS-container">
-                    <input type="password" name="password" placeholder="Enter Password" required />
-                </div>
-                <div>
-                <button type="submit" className="sign-up-button" disabled={navigation.state === "submitting"}>
-                    {navigation.state === "submitting" ? "Signing up..." : "Sign up"}
-                </button>
-                </div>
-                <div className = "log-in">
-                    Already have an account? &nbsp;
-                    <Link to="/login" className="login-link">Login</Link>
-                    </div>
-            </Form>
-            {clientError && (
-                <div>
-                    <p>Error: {clientError}</p>
-                </div>
-            )}
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            height: "100vh"
+        }}>
+            <Image width="400px" style={{ textAlign: "center", paddingBottom: 30 }} src="logo.png" />
+            <Card width="400px">
+                <Text h3 style={{ textAlign: "center" }}>Sign Up</Text>
+                <Form method="post" onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: 10 }}>
+                    <Input name="first_name" placeholder="First Name" required width="100%" crossOrigin={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} />
+                    <Input name="last_name" placeholder="Last Name" required width="100%" crossOrigin={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} />
+                    <Input name="email" htmlType="email" clearable placeholder="Email" required width="100%" crossOrigin={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} />
+                    <Input.Password name="password" clearable placeholder="Password" required width="100%" />
+                    <Button htmlType="submit" type="secondary" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>Sign up</Button>
+                </Form>
+                {clientError && <Text type="error" marginTop="20px">{clientError}</Text>}
+                <Link to="/login"><Text p>Go back</Text></Link>
             </Card>
         </div>
     );
