858061f41166f130a4b9666315e5d9addc44bd3d
adendiamond <adendiamond1@gmail.com>
2024-10-10 14:58:47 +1100

    fixed merge conflict

--- app/routes/app.accounts.tsx
+++ app/routes/app.accounts.tsx
@@ -1,13 +1,12 @@
-import { Card, Grid, Spacer } from '@geist-ui/core';
-import { Account } from '@prisma/client';
-import { json, LoaderFunction } from "@remix-run/cloudflare";
-import { useLoaderData } from "@remix-run/react";
 import React from 'react';
-import { getPrismaClient } from '~/util/db.server';
-import { createUser } from '~/util/userUtil';
-import { getUserSession } from "../auth.server";
+import { useLoaderData } from "@remix-run/react";
+import { json, LoaderFunction } from "@remix-run/cloudflare";
+import { Text, Spacer, Grid, Card } from '@geist-ui/core';
 import AccountCard from '../components/AccountCard';
-import ResizableText from '../components/ResizableText';
+import { Account } from '@prisma/client';
+import { getPrismaClient } from "../util/db.server";
+import { createUser } from '~/util/userUtil';
+import { getUserSession } from '~/auth.server';
 
 type MeUser = {
   uid: string;
@@ -22,102 +21,104 @@ type MeUser = {
 };
 
 export const loader: LoaderFunction = async ({ context, request }: { context: any, request: Request }) => {
-  try {
-    const user = await getUserSession(context, request);
-    const db = getPrismaClient(context);
+  // Ensure the user is authenticated
+  const user = await getUserSession(context, request);
+  const db = getPrismaClient(context);
 
-    if (!user) {
-      throw new Response("Unauthsdorized", { status: 401 });
-    } else {
-      console.log("User found!", user);
-    }
+  if (!user) return json({ error: 'Unauthenticated' }, { status: 401 });
 
-    const getMeUser = async () => {
-      return await Promise.all([
-        db.user.findUnique({
-          where: { uid: user.uid },
-          include: {
-            notifications: {
-              where: { read: false },
-              take: 5,
-            },
+  // Fetch the user details and related data from Prisma
+  // eslint-disable-next-line prefer-const
+  const getMeUser = async () => {
+    return await Promise.all([
+      db.user.findUnique({
+        where: { uid: user.uid },
+        include: {
+          notifications: {
+            where: { read: false },
+            take: 5,
           },
         },
+        // @ts-ignore
+        cacheStrategy: { swr: 60, ttl: 60 },
       }),
       db.account.findMany({
         where: { uid: user.uid },
+        // @ts-ignore
+        cacheStrategy: { swr: 60, ttl: 60 },
       }),
     ]);
-
-    // If user data doesn't exist, handle it accordingly
-    if (!userData) {
-      console.error("User not found!");
-      return json({ error: 'User not found' }, { status: 404 });
-    }
-
-    return json({
-      me: {
-        uid: userData.uid,
-        first_name: userData.first_name,
-        last_name: userData.last_name,
-        email: userData.email,
-        notifications: userData.notifications,
-      },
-      userAccounts: userAccounts,
-    });
-  } catch (error) {
-    console.error(error);
-    return json({ error: 'Failed to fetch user data' }, { status: 500 });
   }
+
+  let [userData, userAccounts] = await getMeUser();
+
+  if (!userData) {
+    console.error("User, not found! Creating new user..", user)
+    await createUser(context, user.uid, user.email, "Plan", "B");
+    [userData, userAccounts] = await getMeUser();
+  }
+
+  userData = userData!
+
+  return json({
+    me: {
+      uid: userData.uid,
+      first_name: userData.first_name,
+      last_name: userData.last_name,
+      email: userData.email,
+      notifications: userData.notifications,
+    },
+    userAccounts,
+  });
 };
 
 export default function Dashboard() {
-  const loaderData = useLoaderData<{
+  const { me: user, userAccounts: accounts } = useLoaderData<{
     me: MeUser;
-    userAccounts: Array<Account & { payID: string }>;
-  } | null>();
+    userAccounts: Account[];
+  }>();
 
-  if (loaderData) {
-    const { me: user, userAccounts: accounts } = loaderData;
+  const totalBalance = accounts.reduce((sum: any, account: { balance: any; }) => sum + account.balance, 0);
 
-    const totalBalance = accounts.reduce((sum: any, account: { balance: any; }) => sum + account.balance, 0);
+  return (
+    <>
+      <Spacer h={2} />
 
-    return (
-      <>
-        <Card padding={1}>
-          <ResizableText style={{ color: 'black' }} small>{new Date().toLocaleDateString('en-US', { day: 'numeric', month: 'long', year: 'numeric' })}</ResizableText>
-          <ResizableText style={{ color: 'black' }} h2>Hi {user.first_name}</ResizableText>
-          <ResizableText small>Next scheduled payment is in <ResizableText b>3 days</ResizableText></ResizableText>
-        </Card>
+      <Text small>{new Date().toLocaleDateString('en-US', { day: 'numeric', month: 'long', year: 'numeric' })}</Text>
+      <Text h2>Hi {user.first_name}</Text>
 
-        <Spacer h={2} />
+      <Spacer h={1} />
 
-        {accounts.map((account) => (
-          <React.Fragment key={account.acc}>
-            <AccountCard
-              accountType={account.short_description}
-              bsb={account.bsb.toString()}
-              accountNumber={account.acc.toString()}
-              payID={account.pay_id ?? undefined}
-              balance={`$${account.balance.toFixed(2)}`}
-            />
-            <Spacer h={1} />
-          </React.Fragment>
-        ))}
+      <Card>
+        <Text small>Next scheduled payment is in <Text b>3 days</Text></Text>
+      </Card>
 
-        <Spacer h={2} />
+      <Spacer h={2} />
 
-        <Card width="100%">
-          <Grid.Container gap={2} justify="space-between" alignItems="center">
-            <Grid>
-              <ResizableText h3>Total</ResizableText >
-            </Grid>
-            <Grid>
-              <ResizableText h3>${totalBalance.toFixed(2)}</ResizableText>
-            </Grid>
-          </Grid.Container>
-        </Card>
-      </>
-    );
-  }
+      {accounts.map((account: { acc: React.Key; short_description: string; bsb: { toString: () => string; }; balance: number; }) => (
+        <React.Fragment key={account.acc}>
+          <AccountCard
+            accountType={account.short_description}
+            bsb={account.bsb.toString()}
+            accountNumber={account.acc.toString()}
+            balance={`$${account.balance.toFixed(2)}`}
+          />
+          <Spacer h={1} />
+        </React.Fragment>
+      ))}
+
+      <Spacer h={2} />
+
+      <Card width="100%">
+        <Grid.Container gap={2} justify="space-between" alignItems="center">
+          <Grid>
+            <Text h3>Total</Text>
+          </Grid>
+          <Grid>
+            <Text h3>${totalBalance.toFixed(2)}</Text>
+          </Grid>
+        </Grid.Container>
+      </Card>
+    </>
+  );
 }
