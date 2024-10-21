ee0639423f4fdf772dbe0cd6bce71c6693c3002b
adendiamond <adendiamond1@gmail.com>
2024-10-21 23:07:27 +1100

    added bypass admin and column sorting

--- learn-to-bank-admin-console/app/middleware/adminMiddleware.ts
+++ learn-to-bank-admin-console/app/middleware/adminMiddleware.ts
@@ -3,7 +3,7 @@ import { getUserSession } from "~/auth.server";
 export async function adminMiddleware(request: Request, context: any): Promise<Response> {
   const userSession = await getUserSession(context, request);
 
-  if (!userSession || userSession.role !== 'administrator') {
+  if (!userSession || (userSession.role !== 'administrator' && !userSession.bypassAdmin)) {
     return new Response("Unauthorized. Administrator access required.", { status: 403 });
   }
 
