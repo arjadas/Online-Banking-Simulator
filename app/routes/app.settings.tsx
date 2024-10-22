07e9998dc798bee410efc4c36d1e42bb5f17429b
TharathepC <80085593+TharathepC@users.noreply.github.com>
2024-10-22 15:25:04 +1100

    feat: account deletion confirmation and enable connection to database

--- app/routes/app.settings.tsx
+++ app/routes/app.settings.tsx
@@ -10,6 +10,7 @@ import { ActionFunction, LoaderFunction, json } from '@remix-run/cloudflare';
 import { getPrismaClient } from '~/service/db.server';
 import { getUserSession } from '../auth.server';
 import { sendResetPasswordEmail } from "~/auth.client";
+import { deleteUser } from "firebase/auth";
 import { User } from '@prisma/client';
 
 type ActionData = {
@@ -22,7 +23,7 @@ export const action: ActionFunction = async ({ context, request } : { context: a
   const user = await getUserSession(context, request);
   const db = getPrismaClient(context);
 
-  const actionType = formData.get('actionType');
+  const actionType = formData.get("actionType");
 
   if (actionType === "saveSettings"){
     const first_name = formData.get("firstName") as string;
@@ -30,42 +31,41 @@ export const action: ActionFunction = async ({ context, request } : { context: a
     const font_preference = formData.get("fontSize") as string;
     const theme_preference = formData.get("theme") as string;
 
-    // try {
-    //   await db.user.update({
-    //     where: { uid: user!.uid },
-    //     data: { first_name, last_name, font_preference},
-    //   });
+    try {
+      await db.user.update({
+        where: { uid: user!.uid },
+        data: { first_name, last_name, font_preference},
+      });
 
-    //   return json ({ success: "New settings saved" });
-    // } catch(error: any) {
-    //   return json ({ error: error.message });
-    // }
-    return json ({ success: "New settings saved" });
+      return json ({ success: "New settings saved" });
+    } catch(error: any) {
+      return json ({ error: error.message });
+    }
   }
 
   if (actionType === "sendEmail") {
     const email = formData.get("email") as string;
 
-    // try {
-    //   await sendResetPasswordEmail(email);
-    //   return json ({ success: "Password reset email sent!" });
-    // } catch (error: any) {
-    //   return json ({ error: error.message });
-    // }
-    return json ({ success: "Password reset email sent!" });
+    try {
+      await sendResetPasswordEmail(email);
+      return json ({ success: "Password reset email sent!" });
+    } catch (error: any) {
+      return json ({ error: error.message });
+    }
   }
 
   if (actionType === "deleteAccount") {
-    return null;
+    deleteUser(user!.uid);
+    return json ({ success: "Account Deleted" });
   }
-  return null;
+  return json ({ error: "Action error" });
 };
 
 export const loader: LoaderFunction = async ({ context, request }: { context: any, request: Request }) => {
   const user = await getUserSession(context, request);
   const db = getPrismaClient(context);
 
-  if (!user) return json({ error: 'Unauthenticated' }, { status: 401 });
+  if (!user) return json({ error: "Unauthenticated" }, { status: 401 });
 
   const [userData] = await Promise.all([
       db.user.findUnique({
@@ -91,9 +91,11 @@ export default function Settings() {
   const { textScale } = useSelector((state: RootState) => state.app);
   const [previewFont, setPreviewFont] = useState(textScale);
   const [modalState, setModalState] = useState(false);
-  // const [passwordError1, setPasswordError1] = useState<string | null>(null);
-  // const [passwordError2, setPasswordError2] = useState<string | null>(null);
   const [endResponse, setEndResponse] = useState(true);
+  const [themeSelect, setThemeSelect] = useState({ option: "light" });
+  const [deleteInput, setDeleteInput] = useState("");
+  const [deleteButton, setDeleteButton] = useState(false);
+  const [deleteError, setDeleteError] = useState(false);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -101,6 +103,7 @@ export default function Settings() {
 
     formData.append("actionType", "saveSettings");
     formData.append("fontSize", String(previewFont));
+    formData.append("theme", String(themeSelect));
 
     submit(formData, { method: "post" });
     dispatch(setTextScale(previewFont));
@@ -110,16 +113,21 @@ export default function Settings() {
   const handleSendEmail = async (event: React.FormEvent) => {
     event.preventDefault();
     const formData = new FormData();
+
+    formData.append("actionType", "sendEmail");
     formData.append("userId", user.uid);
-    formData.append("actionType", "sendEmail")
     formData.append("email", user.email);
     submit(formData, { method: "post" });
     setEndResponse(false);
   }
 
-  const handleDeleteAccount = async (event: React.FormEvent) => {
-    event.preventDefault();
+  const handleDeleteAccount = () => {
+    
+    console.log("handling deletion");
     const formData = new FormData();
+
+    formData.append("actionType", "deleteAccount")
+    submit(formData, { method: "post" });
   }
 
   const previewText = (value: number | number[]) => {
@@ -131,20 +139,54 @@ export default function Settings() {
   const openModal = () => setModalState(true);
   const closeModal = () => setModalState(false);
 
+  const checkText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+
+    setDeleteInput(value);
+
+    if (value === "DELETE") {
+      setDeleteButton(true);
+      setDeleteError(false);
+    } else {
+      setDeleteButton(false);
+    }
+  }
+
+  const checkTextOnBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    const {name, value} = event.target;
+    
+    if (value === "DELETE") {
+      setDeleteButton(true);
+      setDeleteError(false);
+    } else {
+      setDeleteError(true);
+    }
+
+  }
+
+  const handleThemeSelect = (value: string | string[]) => {
+    if (typeof value === "string") {
+      setThemeSelect({ ...themeSelect, option: value });
+    }
+  }
+
   return (
     <Page>
       <Page.Content style={{ display: "flex", justifyContent: "center"}}>
         <Card style={{ width: "550px"}}>
-          <Text h2>Settings</Text>
+          <ResizableText h2>Settings</ResizableText>
           <Form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: 16 }}>
             <Grid.Container style={{ display: "flex", width: "100%", flexDirection: "column", gap: 16 }}>
               <Grid>
-                <Text h5>Edit Name</Text>
+                <ResizableText h5>Edit Name</ResizableText>
                 <Input
                   name="firstName"
+                  htmlType="text"
                   placeholder="First Name"
                   initialValue={user.first_name}
                   width="100%" 
+                  height={`${textScale}px * 2` }
+                  font={`${textScale}px`}
                   clearable
                   required 
                   crossOrigin={undefined} 
@@ -154,9 +196,12 @@ export default function Settings() {
                 <Spacer h={1}/>
                 <Input
                   name="lastName"
+                  htmlType="text"
                   placeholder="Last Name"
                   initialValue={user.last_name}
                   width="100%" 
+                  height={`${textScale}px * 2` }
+                  font={`${textScale}px`}
                   clearable
                   required 
                   crossOrigin={undefined} 
@@ -164,26 +209,28 @@ export default function Settings() {
                   onPointerLeaveCapture={undefined} 
                 />
               </Grid>
+
               <Grid>
-                <Text h5>Change Password</Text>
-                <Button auto onClick={handleSendEmail} style={{ width: "100%"}}
+                <ResizableText h5>Change Password</ResizableText>
+                <Button auto onClick={handleSendEmail} style={{ width: "100%", height:`${textScale}px * 2`, fontSize:`${textScale}px` }}
                   placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
                 >
                   Reset password via Email
                 </Button>
                 {!endResponse && actionData?.error && <ResizableText type="error">{actionData.error}</ResizableText>}
                 {!endResponse && actionData?.success && <ResizableText type="success">{actionData.success}</ResizableText>}
-                {/*passwordError2 && <ResizableText type="error" style={{ marginTop: 10 }}>{passwordError2}</ResizableText>*/}
               </Grid>
+
               <Grid>
-                <Text h5>Change Theme</Text>
-                <Select name="theme" initialValue={"light"}>
-                  <Select.Option value="light">Light</Select.Option>
-                  <Select.Option value="dark">Dark</Select.Option>
+                <ResizableText h5>Change Theme</ResizableText>
+                <Select onChange={handleThemeSelect} initialValue={"light"} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} >
+                  <Select.Option style={{ fontSize:`${textScale}px` }} value="light">Light</Select.Option>
+                  <Select.Option style={{ fontSize:`${textScale}px` }} value="dark">Dark</Select.Option>
                 </Select>
               </Grid>
+
               <Grid>
-                <Text h5>Change Text Size</Text>
+                <ResizableText h5>Change Text Size</ResizableText>
                 <Spacer h={1}/>
                 <Slider
                   min={10}
@@ -195,13 +242,13 @@ export default function Settings() {
                 />
                 <Spacer h={1}/>
                 <Card style={{ fontSize: `${previewFont}px` }}>
-                  <Text h3>Preview</Text>
+                  <ResizableText h3>Preview</ResizableText>
                   <Text>
                     This is a sample text. Adjust the slider above to change its size!
                   </Text>
                 </Card>
               </Grid>
-              <Button type="success" htmlType="submit"
+              <Button type="success" htmlType="submit" style={{ width: "100%", height:`${textScale}px * 2`, fontSize:`${textScale}px` }}
                 placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
               >
                 Save Settings
@@ -211,33 +258,39 @@ export default function Settings() {
           {endResponse && actionData?.error && <ResizableText type="error">{actionData.error}</ResizableText>}
           {endResponse && actionData?.success && <ResizableText type="success">{actionData.success}</ResizableText>}
           <Spacer h={1}/>
-          <Button auto onClick={openModal} type="error" ghost style={{ width: "100%"}}
+          <Button auto onClick={openModal} type="error" ghost style={{ width: "100%", height:`${textScale}px * 2`, fontSize:`${textScale}px` }}
             placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
           >
             Delete Account
           </Button>
           <Modal visible={modalState} onClose={closeModal}>
             <Modal.Title>Delete Account</Modal.Title>
-            <Modal.Subtitle>To confirm, type "DELETE"</Modal.Subtitle>
             <Modal.Content>
               <Form>
-                <p>To confirm, type "DELETE"</p>
+                <ResizableText>To confirm, type <span style={{ fontWeight: "bold"}}>"DELETE"</span></ResizableText>
                 <Input
                     name="delete"
+                    htmlType="text"
                     placeholder="type here"
                     width="100%" 
+                    height={`${textScale}px * 2` }
+                    font={`${textScale}px`}
                     clearable
-                    required 
+                    required
+                    value={deleteInput}
+                    onChange={checkText}
+                    onBlur={checkTextOnBlur}
                     crossOrigin={undefined} 
                     onPointerEnterCapture={undefined} 
                     onPointerLeaveCapture={undefined}
-                    
                 />
               </Form>
-              <p>Warning: Account deletion is permanent and irreversible—once deleted, all data will be lost!</p>
+              {deleteError && <ResizableText style={{ color:"red" }}>type <span style={{ fontWeight: "bold"}}>"DELETE"</span> to enable delete button</ResizableText>}
+              <ResizableText> <span style={{ color: "red", fontWeight: "bold" }}>Warning:</span> Account deletion is permanent and irreversible—once deleted, all data will be lost!</ResizableText>
             </Modal.Content>
-            <Modal.Action passive onClick={() => setModalState(false)} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>Cancel</Modal.Action>
-            <Modal.Action placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>Submit</Modal.Action>
+            <Modal.Action passive onClick={() => setModalState(false)} style={{ fontSize:`${textScale}px` }} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>Cancel</Modal.Action>
+            {!deleteButton && <Modal.Action style={{ backgroundColor: "grey", color: "white", fontSize:`${textScale}px` }} disabled placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>Delete Account</Modal.Action>}
+            {deleteButton && <Modal.Action style={{ backgroundColor: "red", color: "white", fontSize:`${textScale}px` }} onClick={handleDeleteAccount} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>Delete Account</Modal.Action>}
           </Modal>
         </Card>
       </Page.Content>
