291b66b1d29a2cdcf1ca60bca065cb991a0a34a9
arjadas <arjadas17@gmail.com>
2024-10-30 00:59:53 +1100

    rfix: the link to send email for forget password works now

--- app/routes/forgotPassword.tsx
+++ app/routes/forgotPassword.tsx
@@ -4,7 +4,7 @@ import { Form, useActionData } from "@remix-run/react";
 import { sendResetPasswordEmail } from "~/auth.client";
 import AuthenticatedLink from '~/components/AuthenticatedLink';
 
-type ActionData = {
+/*type ActionData = {
   error?: string;
   success?: string;
 };
@@ -20,9 +20,28 @@ export const action: ActionFunction = async ({ request } : { request: Request })
     return json<ActionData>({ error: error.message });
   }
 };
+*/
+
+// firebase auth is handled client-side and not server-side
+const handleSendEmail = async (event: React.FormEvent) => {
+  event.preventDefault();
+  
+  const formData = new FormData(event.target as HTMLFormElement);
+  const email = formData.get("email") as string;
+
+  try {
+    await sendResetPasswordEmail(email);
+    // Show success message
+    alert("Password reset email has been sent!");
+    
+  } catch (error: any) {
+    // Handle error
+    console.error('Password reset error:', error);
+  }
+}
 
 export default function ForgotPassword() {
-  const actionData = useActionData<ActionData>();
+  //const actionData = useActionData<ActionData>();
 
   return (
     <div style={{
@@ -36,7 +55,7 @@ export default function ForgotPassword() {
       <Card width="400px" style={{ padding: 10 }}>
         <Text h3>Forgot Password</Text>
         <Text h6>Email</Text>
-        <Form method="post" style={{ display: "flex", flexDirection: "column", gap: 16 }}>
+        <Form onSubmit={handleSendEmail} style={{ display: "flex", flexDirection: "column", gap: 16 }}>
           <Input name="email" htmlType="email" clearable placeholder="Enter your Email" required width="100%" crossOrigin={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} />
           <Button 
             htmlType="submit" 
@@ -45,8 +64,8 @@ export default function ForgotPassword() {
             Reset Password
           </Button>
         </Form>
-        {actionData?.error && <p style={{ color: "red" }}>{actionData.error}</p>}
-        {actionData?.success && <p style={{ color: "green" }}>{actionData.success}</p>}
+        {/*actionData?.error && <p style={{ color: "red" }}>{actionData.error}</p>*/}
+        {/*actionData?.success && <p style={{ color: "green" }}>{actionData.success}</p>*/}
         <AuthenticatedLink to="/login" prefetch='render'>
           <Text p>Back to Sign in</Text>
         </AuthenticatedLink>
